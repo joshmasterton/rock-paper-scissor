06acbbf35b46831f14710f50b14c83e5f6f03e3d
joshmasterton <joshmasterton@outlook.com>
2023-06-05 23:55:56 +0100

    play round and find winner

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+function gameBoard()
+{
+    const root = document.getElementById("root");
+    const result = document.createElement("div");
+    const choice = document.createElement("div");
+    result.id = "result";
+    choice.id = "choice";
+    root.appendChild(result);
+    root.appendChild(choice);
+
+    const rockBtn = document.createElement("button");
+    const paperBtn = document.createElement("button");
+    const scissorBtn = document.createElement("button");
+    rockBtn.className = "btn";
+    paperBtn.className = "btn";
+    scissorBtn.className = "btn";
+    rockBtn.innerHTML = "rock";
+    paperBtn.innerHTML = "paper";
+    scissorBtn.innerHTML = "scissor";
+    rockBtn.addEventListener("click", (event) => 
+    {
+        playRound(event, comChoice, userChoice, winner);
+    });
+    paperBtn.addEventListener("click", (event) => 
+    {
+        playRound(event, comChoice, userChoice, winner);
+    });
+    scissorBtn.addEventListener("click", (event) => 
+    {
+        playRound(event, comChoice, userChoice, winner);
+    });
+    choice.appendChild(rockBtn);
+    choice.appendChild(paperBtn);
+    choice.appendChild(scissorBtn);
+
+    const comChoice = document.createElement("div");
+    const userChoice = document.createElement("div");
+    const winner = document.createElement("div");
+    result.appendChild(comChoice);
+    result.appendChild(userChoice);
+    result.appendChild(winner);
+};
+
 function getComChoice()
 {
     const randomNum = Math.floor(Math.random() * 3);
@@ -31,30 +74,30 @@ function getUserChoice(event)
     };
 };
 
-function gameBoard()
+function playRound(event, comElement, userElement, winner)
 {
-    const root = document.getElementById("root");
-    const result = document.createElement("div");
-    const choice = document.createElement("div");
-    result.id = "result";
-    choice.id = "choice";
-    root.appendChild(result);
-    root.appendChild(choice);
+    const comChoice = getComChoice();
+    const userChoice = getUserChoice(event);
 
-    const rockBtn = document.createElement("div");
-    const paperBtn = document.createElement("button");
-    const scissorBtn = document.createElement("button");
-    rockBtn.className = "btn";
-    paperBtn.className = "btn";
-    scissorBtn.className = "btn";
-    choice.appendChild(rockBtn);
-    choice.appendChild(paperBtn);
-    choice.appendChild(scissorBtn);
-
-    const comChoice = document.createElement("div");
-    const userChoice = document.createElement("div");
-    result.appendChild(comChoice);
-    result.appendChild(userChoice);
+    comElement.innerHTML = `computer chose ${comChoice}`;
+    userElement.innerHTML = `you chose ${userChoice}`;
+    
+    if(comChoice === "rock" && userChoice === "scissor" ||
+    comChoice === "paper" && userChoice === "rock" ||
+    comChoice === "scissor" && userChoice === "paper")
+    {
+        winner.innerHTML = "computer wins";
+    }
+    else if(comChoice === "scissor" && userChoice === "rock" ||
+    comChoice === "rock" && userChoice === "paper" ||
+    comChoice === "paper" && userChoice === "scissor")
+    {
+        winner.innerHTML = "users wins";
+    }
+    else
+    {
+        winner.innerHTML = "draw";
+    }
 };
 
 gameBoard();
